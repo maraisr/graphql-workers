c85989d94b50c76f053d5312087462c35f79a5f5
Marais Rossouw <me@marais.co>
2021-12-11 16:15:54 +1000

    feat: init

---
+++ readme.md
@@ -0,0 +1,61 @@
+# graphql-workers
+
+<div align="right">
+<p><code>npm add graphql-workers</code> puts graphql on the edge</p>
+<span>
+<a href="https://github.com/maraisr/graphql-workers/actions/workflows/ci.yml">
+	<img src="https://github.com/maraisr/graphql-workers/actions/workflows/ci.yml/badge.svg"/>
+</a>
+<a href="https://npm-stat.com/charts.html?package=graphql-workers">
+	<img src="https://badgen.net/npm/dw/graphql-workers?labelColor=black&color=black&cache=600" alt="downloads"/>
+</a>
+<a href="https://bundlephobia.com/result?p=graphql-workers">
+	<img src="https://badgen.net/bundlephobia/minzip/graphql-workers?labelColor=black&color=black" alt="size"/>
+</a>
+</span>
+
+<br />
+<br />
+</div>
+
+## ⚡ Features
+
+- 🤔 **Familiar** — thin layer ontop of graphql-js
+
+- 🪢 **defer/stream** — incremental delivery out of the box
+
+## 🚀 Usage
+
+```sh
+npm add graphql-workers \
+  graphql@16.1.0-experimental-stream-defer.6
+```
+
+> at this stage you need a custom tag to support defer/stream, but this is optional
+
+```ts
+import { makeHandler, createSchemaResponder } from 'graphql-workers';
+
+// your schem as however you have it defined
+declare const schema: GraphQLSchema;
+
+export default {
+	async fetch(req, env, ctx) {
+		const { reply } = createSchemaResponder(ctx, schema);
+
+		// ~> get the paramaters in way you wish
+		// maybe its a KV read for Relay persisted queries
+		const { query, variables, operationName } = await req.json();
+
+		// ~> then simply reply
+		return reply(query, variables, operationName);
+	}
+}
+
+// Or a module workers convience method
+export default makeHandler(schema);
+```
+
+## License
+
+MIT © [Marais Rossouw](https://marais.io)
---
+++ src/index.ts
@@ -0,0 +1,90 @@
+import flru from "flru";
+import {
+	type DocumentNode,
+	execute,
+	type GraphQLSchema,
+	parse,
+	validate,
+	ValidationRule,
+} from "graphql";
+import {stream} from "piecemeal/worker";
+import { send } from "worktop/response";
+
+export interface Options {
+	context?: unknown;
+	validationRules?: ValidationRule[];
+	cacheQueryParse?: boolean;
+	executor?: typeof execute;
+}
+
+let queryCache = flru<DocumentNode>(10);
+
+export const createSchemaResponder = (ctx: ExecutionContext, schema: GraphQLSchema, options?: Options) => {
+	options = options || {};
+
+	let cache = (options.cacheQueryParse ?? true) ? queryCache : null;
+
+	async function reply(query: DocumentNode | string, variables?: Record<string, any>, operationName?: string) {
+		let query_ast: DocumentNode,
+			query_string = typeof query === "string";
+
+		if (query_string) {
+			if (cache && cache.has(query as string)) {
+				query_ast = cache.get(query as string);
+			} else {
+				query_ast = parse(query as string);
+			}
+		} else {
+			query_ast = query as DocumentNode;
+		}
+
+		const validation_errors = validate(schema, query_ast, options.validationRules, {
+			maxErrors: 1,
+		});
+		if (validation_errors.length) return send(406, { errors: validation_errors });
+		if (query_string && cache) cache.set(query as string, query_ast);
+
+		const result = await (options.executor || execute)({
+			operationName,
+			schema,
+			document: query_ast,
+			contextValue: options.context,
+			variableValues: variables,
+		});
+
+		if (isAsyncGenerator(result)) {
+			const { response, pipe } = stream(result);
+			ctx.waitUntil(pipe());
+			return response;
+		}
+
+		return send(200, result, {
+			"content-type": "application/graphql+json",
+		});
+	}
+
+	return { reply };
+};
+
+export const makeHandler = (schema: GraphQLSchema, options?: Options): ExportedHandler => ({
+	fetch: async (req, _env, ctx) => {
+		try {
+			var body: { query: string, operationName?: string, variables?: Record<string, any> } = await req.json();
+		} catch (e) {
+			return send(406, { errors: [{ message: "malformed body" }] });
+		}
+
+		const query = body.query;
+		if (!query) return send(406, { errors: [{ message: "query param required" }] });
+
+		const { reply } = createSchemaResponder(ctx, schema, options);
+
+		return reply(query, body.variables, body.operationName);
+	},
+});
+
+const isAsyncGenerator = (input: unknown): input is AsyncGenerator =>
+	typeof input === "object" &&
+	input !== null &&
+	((input as any)[Symbol.toStringTag] === "AsyncGenerator" ||
+		(Symbol.asyncIterator && Symbol.asyncIterator in input));
